24babd33cfd073bc937c6b84fe72ea25f60458b6
AdilsonRocha-coder <piuwave@hotmail.com>
2021-02-10 17:49:23 -0300

    Initial setup

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.devsuperior.dsdeliver.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private Environment env;
+	
+	
+	//abaixo disabilitamos a proteção contra CSRF, que é um tipo de ataque baseado em sessão
+	//como nossa aplicação é baseada em rest e ele não trabalha em sessão, não precisamos esse
+	//tipo de proteção e liberamos todos os tipos de requisições
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+		
+		http.cors().and().csrf().disable();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.authorizeRequests().anyRequest().permitAll();
+	}
+
+	//abaixo esse código permite a comunicação entre o back-end, que está no Heroku e o front-end 
+	//que está em outro domínio ( netlifly ou expo ) não tenho certeza e depois corrijo aqui.
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+}
